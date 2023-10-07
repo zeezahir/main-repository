459e525eaaf44c819eb2237b35e31bd3a7e5c081
zeezahir <zeezahirzee@gmail.com>
2023-10-07 13:40:05 +0000

    Commit-New-Branch-Dev

--- git-practice-1/git-demo-1.sh
+++ git-practice-1/git-demo-1.sh
@@ -2,3 +2,4 @@
 echo "My 1st line added after git initilization"
 echo "2nd line added after git clone command"
 echo "Line added and went for namaz, will come back and upload the project again"
+echo "New line added after Devv branch creation"
