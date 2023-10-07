d3d006df9f537bf1bc699aeb47ccce360650d900
zeezahir <zeezahirzee@gmail.com>
2023-10-07 13:30:21 +0000

    Commit-After Git-Stash

--- git-practice-1/git-demo-1.sh
+++ git-practice-1/git-demo-1.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "My 1st line added after git initilization"
 echo "2nd line added after git clone command"
+echo "Line added and went for namaz, will come back and upload the project again"
